5b632124eb33febf5c4a9800076aa2ce2b6d1394
michaelyananimoto <michael.yan@animoto.com>
2016-12-06 13:58:36 -0500

    Define active hackimoto

--- app/models/hackimoto.rb
+++ app/models/hackimoto.rb
@@ -3,4 +3,7 @@ class Hackimoto < ActiveRecord::Base
   has_many :hackimoto_admin_users
   has_many :hacks
   has_many :admin_users, :through => :hackimoto_admin_users, :source => :user
+
+  scope :active, lambda { last }
+
 end
