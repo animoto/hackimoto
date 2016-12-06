f33ef303aa57c4632a652f6b20a72f0687d5feef
Jay Sirju <jay.sirju@animoto.com>
2016-12-06 11:41:57 -0500

    Allow for signin/out using Google Auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.email
-    #redirect_to root_path
+    redirect_to hackimotos_path
   end
 
   def destroy
     session[:user_id] = nil
-    #redirect_to root_path
+    redirect_to hackimotos_path
   end
 end
