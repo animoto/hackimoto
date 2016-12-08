a8e0cc82ddcc8530afaa81a2f04646ec8f23768a
Norris Nakagaki <norrisnakagaki@gmail.com>
2016-12-08 12:52:49 -0500

    add and remove judges and admins

--- app/controllers/hackimotos_controller.rb
+++ app/controllers/hackimotos_controller.rb
@@ -49,6 +49,40 @@ class HackimotosController < ApplicationController
     redirect_to hackimotos_path
   end
 
+  def add_judge_form
+    @hackimoto = Hackimoto.find(params[:hackimoto_id])
+    render :add_judge
+  end
+
+  def add_judge
+    @hackimoto = Hackimoto.find(params[:hackimoto_id])
+    @hackimoto.judges.create({ user_id: params[:hackimoto][:judges] })
+    redirect_to hackimoto_path(@hackimoto)
+  end
+
+  def remove_judge
+    @hackimoto = Hackimoto.find(params[:hackimoto_id])
+    @hackimoto.judges.find_by(user_id: params[:user_id]).delete
+    redirect_to hackimoto_path(@hackimoto)
+  end
+
+  def add_admin_form
+    @hackimoto = Hackimoto.find(params[:hackimoto_id])
+    render :add_admin
+  end
+
+  def add_admin
+    @hackimoto = Hackimoto.find(params[:hackimoto_id])
+    @hackimoto.hackimoto_admin_users.create({ user_id: params[:hackimoto][:hackimoto_admin_users] })
+    redirect_to hackimoto_path(@hackimoto)
+  end
+
+  def remove_admin
+    @hackimoto = Hackimoto.find(params[:hackimoto_id])
+    @hackimoto.hackimoto_admin_users.find_by(user_id: params[:user_id]).delete
+    redirect_to hackimoto_path(@hackimoto)
+  end
+
   private
 
   def hackimoto_params
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,11 @@ HackimotoApp::Application.routes.draw do
 
   post '/register_to_hackimoto', controller: :users, action: :register_to_hackimoto, as: :register_to_hackimoto
   get '/register_to_hack', controller: :users, action: :register_to_hack, as: :register_to_hack
+
+  get '/add_judge_form/:hackimoto_id', controller: :hackimotos, action: :add_judge_form, as: :add_judge_form
+  get '/add_admin_form/:hackimoto_id', controller: :hackimotos, action: :add_admin_form, as: :add_admin_form
+  patch '/add_judge/:hackimoto_id', controller: :hackimotos, action: :add_judge, as: :add_judge
+  patch '/add_admin/:hackimoto_id', controller: :hackimotos, action: :add_admin, as: :add_admin
+  delete '/remove_judge/:hackimoto_id/:user_id', controller: :hackimotos, action: :remove_judge, as: :remove_judge
+  delete '/remove_admin/:hackimoto_id/:user_id', controller: :hackimotos, action: :remove_admin, as: :remove_admin
 end
